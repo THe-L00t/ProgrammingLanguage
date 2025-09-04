9a64ffab13f0f33ddb6008b9c615eece3e2fb80b
THe-L00t <sigun2001@naver.com>
2025-09-04 18:32:28 +0900

    save파일 꾸미기

--- test/save.cpp
+++ test/save.cpp
@@ -11,6 +11,16 @@ void save(std::string_view fileName)	//code convention:
 
 	std::ofstream out{ "ProgrammingLanguage_BackUp.txt",std::ios::app };
 
+	auto now = std::chrono::system_clock::now();
+	using namespace std::literals;
+	now += 9h;
+
+	out << '\n' << '\n';
+	out << "=====================================================" << std::endl;
+	out << "저장시간:" << now << std::endl;
+	out << "=====================================================" << std::endl;
+	out << '\n';
+
 	char c;
 	while (in >> c)
 		out << c;
