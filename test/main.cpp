d8dd52760570687903dfc8739c7727830a4cc42e
THe-L00t <sigun2001@naver.com>
2025-09-11 19:01:20 +0900

    [문제] int 값 2개를 입력받아라. (함수 문제)

--- test/main.cpp
+++ test/main.cpp
@@ -6,10 +6,20 @@
 
 #include "save.h"
 
+// [문제] int 값 2개를 입력받아라. 
+// 2개의 int를 인자로 받아 합계를 리턴하는 함수 add를 작성하여
+// 합계를 화면에 출력하라
+// main이 문제없이 의도대로 실행되도록 하라
 
 int main()
-{	
-	int n[1000'0000];	//가능할까?
+{
+	int a{}, b{};
+	std::cout << "int 2개를 입력받아 합계를 출력하는 프로그램" << std::endl;
+	std::cout << "값 2개를 입력하세요 - ";
+	std::cin >> a >> b;
+
+	std::cout << a << " + " << b << " = " << add(a, b) << std::endl;
+
 
 	save("main.cpp");
 }
