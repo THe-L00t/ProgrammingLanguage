18b4f77cf7ae6faea33a1a4d9355145e551392dd
THe-L00t <sigun2001@naver.com>
2025-09-11 18:57:38 +0900

    다음 시간에 할 것

--- test/main.cpp
+++ test/main.cpp
@@ -6,18 +6,10 @@
 
 #include "save.h"
 
-int gn;
 
 int main()
 {	
-	int n;
-	float f;
-	int* np = new int{};
-
-	std::cout <<"지역변수 n의 주소 :   " << std::addressof(n) << std::endl;
-	std::cout << "지역변수 f의 주소 :   " << std::addressof(f) << std::endl;
-	std::cout << "전역변수 gn의 주소 :  " << std::addressof(gn) << std::endl;
-	std::cout << "동적할당변수의 주소 : " << np << std::endl;
+	int n[1000'0000];	//가능할까?
 
 	save("main.cpp");
 }
