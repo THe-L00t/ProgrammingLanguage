470cea009c28eee2371f6285149c45311597e773
THe-L00t <sigun2001@naver.com>
2025-09-15 12:17:08 +0900

    초기화 되지 않은 배열을 살펴보자

--- test/main.cpp
+++ test/main.cpp
@@ -6,28 +6,14 @@
 
 #include "save.h"
 
-// [문제] int 값 2개를 입력받아라. 
-// 2개의 int를 인자로 받아 합계를 리턴하는 함수 add를 작성하여
-// 합계를 화면에 출력하라
-// main이 문제없이 의도대로 실행되도록 하라
-
-int add(int&, int&);	// function declaration, prototype, signature
-//	메모리와 무관
-
-int add(int& a, int& b) {	//function definition, 메모리를 차치함
-	return a + b;
-}
 
 int main()
 {
-	int a{}, b{};
-	std::cout << "int 2개를 입력받아 합계를 출력하는 프로그램" << std::endl;
-	std::cout << "값 2개를 입력하세요 - ";
-	std::cin >> a >> b;
+	int n[40];
 
-	std::cout << a << " + " << b << " = " << add(a, b) << std::endl;
-
-	std::cout << std::addressof(add) << std::endl;
+	for (int& i : n) {
+		std::cout << i << std::endl;
+	}
 
 	//save("main.cpp");
 }
