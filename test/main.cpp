d55057a16a2e531ddb0d50c3544108bef9121ed7
THe-L00t <sigun2001@naver.com>
2025-09-22 12:48:01 +0900

    [문제] 전역변수의 최대 크기를 알아본다.

--- test/main.cpp
+++ test/main.cpp
@@ -5,35 +5,15 @@
 #include <iostream>
 
 #include "save.h"
+#define Max 1'800'000'000
 
-// [문제] 지역변수란 무엇인가?	-> 전역변수가 아닌 모든 변수
-// 어디에 위치하나?			-> STACK segment
-// 얼마만큼 쓸 수 있나?		->1 Megabyte
-int cnt{1};
+// [문제] 전역변수의 최대 크기를 알아본다.
+char c[Max]{1,2,3};
 
-void func();
-
-void func() {
-	int n[10'000]{};	//40kb *25 ==> 1mb
-	std::cout << cnt++ << "번 불렸습니다" <<n[9'999]<< std::endl;
-
-	// recursive fuction call 
-	func();
-	std::cout << n[0] << std::endl;
-}
 
 int main()
 {
-	// [문제] 지역변수는 STACK에 위치하는데 프로그램 실행 중에 
-	// 계속 지역변수를 요청하면 언젠가는 더 이상 STACK을 사용할 수 없게 된다.
-	// --> STACK overflow
-	// 그렇게 해보세요
-	
-	/*int num[250*1024]{};
-	for (auto& n : num) {
-		std::cout << n << std::endl;
-	}*/
-	func();
+	std::cout << c[Max-1] << std::endl;
 	
 	//save("main.cpp");
 }
