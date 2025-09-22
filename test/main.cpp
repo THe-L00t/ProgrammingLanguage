a9c9b2d335afd5eb6797a1822e22f0ea8faf53d6
THe-L00t <sigun2001@naver.com>
2025-09-22 11:58:14 +0900

    생명주기 확인

--- test/main.cpp
+++ test/main.cpp
@@ -9,10 +9,27 @@
 // 어디에 위치하나?			-> STACK segment
 // 얼마만큼 쓸 수 있나?		->1 Megabyte
 
+class LCC {
+public:
+	LCC(int n) : num{ n } {
+		std::cout << num << "번객체 생겼어요" << std::endl;
+	}
+	~LCC() {
+		std::cout << num << "번 객체 사라져요" << std::endl;
+	}
+private:
+	int num;
+};
+
 int n{ 123 };
+LCC l(1);
 
 int main()
 {
 	int n{ 1 };
+	{
+		LCC l(2);
+	}
+
 	//save("main.cpp");
 }
