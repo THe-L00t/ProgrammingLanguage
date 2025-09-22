fd4c5a6e0622b6c0a784a014dcd3bddd8fc5ff41
THe-L00t <sigun2001@naver.com>
2025-09-22 12:31:33 +0900

    프로그램 스택오버플로 유도 : 스택 최대 크기는 1mb임을 확인

--- test/main.cpp
+++ test/main.cpp
@@ -8,17 +8,29 @@
 // [문제] 지역변수란 무엇인가?	-> 전역변수가 아닌 모든 변수
 // 어디에 위치하나?			-> STACK segment
 // 얼마만큼 쓸 수 있나?		->1 Megabyte
+int cnt{1};
 
+void func() {
+	int n[10'000]{};	//40kb *25 ==> 1mb
+	std::cout << cnt++ << "번 불렸습니다" <<n[9'999]<< std::endl;
+
+	// recursive fuction call 
+	func();
+	std::cout << n[0] << std::endl;
+}
 
 int main()
 {
-	// [문제] int형 변수를 몇 개까지 만들 수 있는 지 확인하라.
-	// 전체 몇 바이트인지 알아보자. 
-	int n[250*1024];
-	int s{ 0 };
-	for (int& i : n) {
-		std::cout << i << std::endl;
-	}
-	std::cout << sizeof(n)/1024./1024. <<"mb" << std::endl;
+	// [문제] 지역변수는 STACK에 위치하는데 프로그램 실행 중에 
+	// 계속 지역변수를 요청하면 언젠가는 더 이상 STACK을 사용할 수 없게 된다.
+	// --> STACK overflow
+	// 그렇게 해보세요
+	
+	/*int num[250*1024]{};
+	for (auto& n : num) {
+		std::cout << n << std::endl;
+	}*/
+	func();
+	
 	//save("main.cpp");
 }
