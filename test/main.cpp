c39e066373f5089f6c4251437c0666eae4c7e422
THe-L00t <sigun2001@naver.com>
2025-09-29 12:22:03 +0900

    함수 오버로딩(function overloading)

--- test/main.cpp
+++ test/main.cpp
@@ -6,7 +6,15 @@
 
 #include "save.h"
 
+//c++언어는 같은 이름의 함수를 사용할 수 있다. - 함수 오버로딩(function overloading) 
+//void change(int, int); 오류가 발생한다
+void change(int*, int*);
 void change(int&, int&);
+void change(int* a, int* b) {
+	int temp{ *a };
+	*a = *b;
+	*b = temp;
+}
 void change(int& a, int& b) {
 	int temp{ a };
 	a = b;
