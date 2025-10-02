6b6cd68ecd263e8e38abe332c1329d73d363b4f6
THe-L00t <sigun2001@naver.com>
2025-10-02 17:38:27 +0900

    ASCII로 소리 출력 해보기

--- test/main.cpp
+++ test/main.cpp
@@ -11,15 +11,7 @@ char pangram[]{"the quick brown fox jumps over the lazy dog"}; // 문자열 관련 AS
 
 int main()
 {
-	qsort(pangram, sizeof(pangram), sizeof(char), [](const void* a, const void* b) {
-		char A = *(char*)a; char B = *(char*)b;
-		if (A not_eq B) return (A < B) ? -1 : 1;
-		else return 0;
-		});
-	for (const char& c : pangram) {
-		std::cout << c;
-	}
-	std::cout << std::endl;
+	std::cout << '\7';
 	//save("main.cpp");
 }
 
