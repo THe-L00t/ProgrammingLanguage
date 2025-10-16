9d7f9032cbd4982ce5e407928d59ad5fe2238708
THe-L00t <sigun2001@naver.com>
2025-10-16 18:54:37 +0900

    [문제] 소문자의 개수를 세서 다음과 같이 출력하라

--- test/main.cpp
+++ test/main.cpp
@@ -3,40 +3,20 @@
 //------------------------------------------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
-#include <print>
+
 
 #include "save.h"
 
-//[문제] e class에서 다운받은 실행파일을 실행하면 폴더에 
-// "몇개인지모르는int값들.txt" 파일에 생성된다. 
-// 여기에는 정확하게 2000만개의 int값이 저장되어 있다. 
-// 오름차순으로 정렬하라.
-// 정렬된 int값을 화면에 모두 출력하라. 한 개의 int를 20칸에 맞추어라.
-#define mn 2000'0000
- 
-int nums[mn]{};	// 컴파일 타임에 개수 정해짐, 동적할당 x 
+//[문제] 지금 코딩하고 있는 파일은 main.cpp이다. 
+// 소문자의 개수를 세서 다음과 같이 출력하라 
+// a-3
+// b-2 
+// z -0 
+
 
 int main()
 {
-	std::ifstream in{ "몇개인지모르는int값들.txt" };
-	if (not in) return 404;
-
-	//int* nums = new int[mn];
-	int temp{};
-	size_t idx{};
-	while (in >> temp) {
-		nums[idx++] = temp;
-	}
-	qsort(nums, mn,sizeof(int), [](const void* a, const void* b) {
-		return *(int*)a - *(int*)b;
-		});
-
-	for (int& n : nums)
-	{
-		std::print("{:20}", n);
-	}
-
-	//delete[] nums;
+	
 	//save("main.cpp");
 }
 
