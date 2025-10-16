7e37532f186551ab0968f480f58a26ee8c6a2ad5
THe-L00t <sigun2001@naver.com>
2025-10-16 18:59:08 +0900

    개인 문제 풀이

--- test/main.cpp
+++ test/main.cpp
@@ -16,7 +16,20 @@
 
 int main()
 {
-	
+	int num['z' - 'a'+1]{};
+	std::ifstream in{ "main.cpp" };
+	char temp{};
+	int cnt{};
+	while (in >> temp) {
+		if (islower(temp)) {
+			num[temp - 'a']++;
+		} 
+	}
+	char c{ 'a' };
+	for (int& n : num) {
+		std::cout << (char)(c++) << "-" << n << std::endl;
+	}
+
 	//save("main.cpp");
 }
 
