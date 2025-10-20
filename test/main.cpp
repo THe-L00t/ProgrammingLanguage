b9af50c02e8cddb054235550b3546194ea1ecdec
THe-L00t <sigun2001@naver.com>
2025-10-20 12:41:23 +0900

    [문제] 메모리 저장 및 정렬 출력

--- test/main.cpp
+++ test/main.cpp
@@ -8,21 +8,39 @@
 #include "save.h"
 
 //[문제] 지금 코딩하고 있는 파일은 "main.cpp"이다 
-// 이 파일에는 모두 몇 단어가 있는지 출력하라 
-//(단어) 공백으로 분리된 문자 집합을 말한다. 
+// 단어가 모두 몇 개인지 출력하라
+// 여기에 단어를 모두 메모리에 저장하라
+// 오름차순으로 정렬하라 
+// 정렬 결과를 출력하라 
 
 
 int main()
 {
-	std::ifstream in{ "main.cpp" };
-	std::ofstream out{ "대문자MAIN.CPP" };
-	if (not in) return 404;
-	std::string temp{};
-	int cnt{};
-	while (in >> temp) {
-		++cnt;
+	unsigned cnt{}; //
+	{
+		std::ifstream in{ "main.cpp" };
+		if (not in) return 404;
+		std::string temp{};
+		while (in >> temp) {
+			++cnt;
+		}
 	}
-	std::cout << cnt << "개 입니다.";
+	std::ifstream in{ "main.cpp" };
+	if (not in) return 404;
+	// 단어개수가 실시간에 파악되었다
+	// 다음 문제를 해결하라
+	std::string* strings = new std::string[cnt];
+	unsigned idx{};
+	while (in >> strings[idx++]) {
+
+	}
+	qsort(strings, cnt, sizeof(std::string), [](const void* a, const void* b) {
+		return *(std::string*)a < *(std::string*)b? -1 : 1 ;
+		});
+	for (size_t i = 0; i < cnt; i++)
+	{
+		std::cout << strings[i] << std::endl;
+	}
 
 	//save("main.cpp");
 }
