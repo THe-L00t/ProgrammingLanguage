f9b28374ec68c87260c887fbbdd2ad26e860ea7a
THe-L00t(EPC) <sigun2001@naver.com>
2025-11-03 11:34:55 +0900

    Dog dog;돌아가도록 수정하라

--- test/main.cpp
+++ test/main.cpp
@@ -8,22 +8,12 @@
 
 #include "save.h"
 
-struct Point2D { //alignment = 8byte
-	int x{};
-	int y{};
 
-	void show() {	//show(this) : hidden argument,  this : 모든 멤버함수의 첫번째 인자
-		std::cout << "(" << x << ", " << y << ")" << std::endl;
-	}
-};
 
 int main()
 {
-	Point2D points[100]{ {1,2},{123,456} };
+	Dog dog;
 
-	for (Point2D p : points) {
-		p.show();
-	}
 	//save("main.cpp");
 }
 
