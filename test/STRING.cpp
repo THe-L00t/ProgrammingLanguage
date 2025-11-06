0cf2e47e50a96ce895592496647d436ce75a48bb
THe-L00t <sigun2001@naver.com>
2025-11-06 17:51:42 +0900

    using std::cout; 이렇게도 가능

--- test/STRING.cpp
+++ test/STRING.cpp
@@ -1,10 +1,15 @@
+#include <iostream>
 #include "STRING.h"
+
+using std::cout;
+
 //CPP에서 클래스를 정의한다.
 STRING::STRING()
 {
 }
 
-STRING::STRING(const char* c) : len{ strlen(c) } {
+STRING::STRING(const char* c) : len{ strlen(c) } 
+{
 	data = new char[len];
 	memcpy(data, c, len);	//초고속 복사 - DMA
 	std::cout << "생성자" << std::endl;
