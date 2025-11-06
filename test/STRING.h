ff3850a056d1b7830a532a8f3af9f614df9d0018
THe-L00t <sigun2001@naver.com>
2025-11-06 17:54:53 +0900

    special 함수들의 이유

--- test/STRING.h
+++ test/STRING.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <iostream>
 // 헤더에는 클래스를 선언한다
+// 이 클래스는 자원을 확보하고 관리하는 클래스이다.
+// 이 클래스에 special 함수들을 작성해야만 하는 이유를 공부한다. 
+
 class STRING {
 public:
 	STRING();
--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 
+#include "STRING.h"
 #include "save.h"
 
 using namespace std;
