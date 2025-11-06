7a8753a407269b86ebc2631993971f05fd93e815
THe-L00t <sigun2001@naver.com>
2025-11-06 18:00:16 +0900

    && r-value reference

--- test/STRING.h
+++ test/STRING.h
@@ -3,7 +3,18 @@
 // 헤더에는 클래스를 선언한다
 // 이 클래스는 자원을 확보하고 관리하는 클래스이다.
 // 이 클래스에 special 함수들을 작성해야만 하는 이유를 공부한다. 
+/*
+	class 의 special 함수 
+	1. STRING()								: default constructor
+	2. ~STRING()							: destructor 
 
+	3. STRING( const STRING& )				: copy constructor
+	4. STRING& operator=( const STRING& )	: copy assignment operator
+
+	5. STRING( STRING&& )					: move constructor
+	6. 
+*/
+// && r-value reference  
 class STRING {
 public:
 	STRING();
