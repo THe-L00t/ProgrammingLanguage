e8a29ee4d2276671e0f2fcf28f0a5d8d767d904a
THe-L00t <sigun2001@naver.com>
2025-11-13 18:03:55 +0900

    string의 +연산

--- test/main.cpp
+++ test/main.cpp
@@ -15,16 +15,12 @@ extern bool observe;
 
 int main()
 {
-	STRING s[]{ "333", "1", "55555", "4444", "22" };
-	observe = true;
-	//[문제] s를 글자수 기준 오름차순으로 정렬하라
-	qsort(s, sizeof(s) / sizeof(STRING), sizeof(STRING), [](const void* a, const void* b) {
-		return int(((STRING*)a)->length() - ((STRING*)b)->length());
-		});	// 인자를 STRING으로 다시 받아오게되면 복사생성자가 많이 호출, 굳이 그렇게 하고 싶으면 &사용 
+	std::string s1{ "2025년 11월 13일" };
+	std::string s2{ " 오늘은 수능 시험일" };
 	
-	for (const STRING& s : s) {
-		std::cout << s << std::endl;
-	}
+	std::string s3 = s1 + s2;
+
+	std::cout << s3 << std::endl;
 	//save("main.cpp");
 }
 
