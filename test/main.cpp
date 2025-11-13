115234f267df0eb8ecbeb66b65d0a70d35242414
THe-L00t <sigun2001@naver.com>
2025-11-13 18:59:15 +0900

    정상적이라면 복사생성자 4번 등장 후 3번 소멸자. 그러나 컴파일러가 temp를 그대로 s3에 이용

--- test/main.cpp
+++ test/main.cpp
@@ -19,7 +19,7 @@ int main()
 	STRING s1{ "2025년 11월 13일" };
 	STRING s2{ " 오늘은 수능 시험일" };
 	
-	STRING s3 = s1 + s2;		
+	STRING s3 = s1 + s2;	// 정상적이라면 복사생성자 4번 등장 후 3번 소멸자. 그러나 컴파일러가 temp를 그대로 s3에 이용
 
 	std::cout << s3 << std::endl;
 	//save("main.cpp");
