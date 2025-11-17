ebe9d9073aab3a8bb996f45c12e8ae3ffeb7dbcb
THe-L00t <sigun2001@naver.com>
2025-11-17 11:45:45 +0900

    문제 STRING t = "첫번째 내용" + s; //위의 내용을 살려보자

--- test/main.cpp
+++ test/main.cpp
@@ -16,13 +16,11 @@ extern bool observe;
 int main()
 {
 	observe = true;
-	STRING s1{ "2025년 11월 13일" };
-	STRING s2{ " 오늘은 수능 시험일" };
-	
-	STRING s3 = s1 + s2;	// 정상적이라면 복사생성자 4번 등장 후 3번 소멸자. 그러나 컴파일러가 temp를 그대로 s3에 이용
-	// RVO라는 최적화 기법이다.  
+	STRING s{ "두번째 내용" };
+	STRING t = "첫번째 내용" + s;
+	//위의 내용을 살려보자 
 
-	std::cout << s3 << std::endl;
+
 	//save("main.cpp");
 }
 
