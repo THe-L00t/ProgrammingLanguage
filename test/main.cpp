5dfecb67346e0e2febc036d48694ea8c6d9735da
THe-L00t <sigun2001@naver.com>
2025-11-20 18:05:39 +0900

    lvalue 설명

--- test/main.cpp
+++ test/main.cpp
@@ -20,7 +20,7 @@ int main()
 	// [문제] s의 글자중에서 숫자를 찾아 '*'로 바꿔라
 	for (size_t i = 0; i < s.length(); i++)
 	{
-		if (isdigit(s[i])) s[i] = '*';
+		if (isdigit(s[i])) s[i] = '*';	//처음 왜 빨간줄이 뜨나 
 	}
 
 
