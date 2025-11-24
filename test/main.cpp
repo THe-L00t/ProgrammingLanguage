71acd44c668b9ba5656d680dee781a6d4c48bb5c
THe-L00t <sigun2001@naver.com>
2025-11-24 11:47:09 +0900

    [문제] main()이 문제없이 실행되도록 필요한 코딩을

--- test/main.cpp
+++ test/main.cpp
@@ -14,11 +14,18 @@
 using namespace std;
 extern bool observe;
 
+// [문제] main()이 문제없이 실행되도록 필요한 코딩을 
+
 int main()
 {
 	// 연산자 오버로딩 중에서 강의하지 않은 연산자가 있다. () - 함수 호출 연산자
-
 	
+	Dog dogs[10];	// Dog는 생성시 [10,30] 임의의 길이를 갖는 STRING name멤버가 있다. 
+				// name은 생성시 랜덤 소문자로 초기화된다. 
+
+	for (const Dog& dog : dogs) {
+		std::cout << dog << std::endl;
+	}
 
 	
 	//save("main.cpp");
