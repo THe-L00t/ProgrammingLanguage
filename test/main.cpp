9792f822e014502b5ba17b73be3f40534368d858
THe-L00t <sigun2001@naver.com>
2025-11-24 12:30:57 +0900

    함수 호출 연산자 등장

--- test/main.cpp
+++ test/main.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <string>
 #include <random>
+#include <print>
 
 #include "STRING.h"
 #include "save.h"
@@ -16,6 +17,7 @@ extern bool observe;
 std::default_random_engine dre{};
 std::uniform_int_distribution<int> uid{ 'a','z' };
 std::uniform_int_distribution sizeuid{ 10,30 };
+std::uniform_int_distribution rid{ 1,100'0000 };
 
 // [문제] main()이 문제없이 실행되도록 필요한 코딩을 추가하라
 class Dog {
@@ -30,7 +32,13 @@ public:
 		temp[nSize] = '\0';
 		name = temp;
 		delete[] temp;
+		id = rid(dre);
 	}
+	void operator()() const{
+		std::print("[{:8}] - ", id);
+		std::cout << name << std::endl;
+	}
+
 	size_t nameSize() const {
 		return name.length();
 	}
@@ -39,6 +47,7 @@ public:
 	}
 private:
 	STRING name;
+	int id;
 };
 
 int main()
@@ -46,17 +55,18 @@ int main()
 	// 연산자 오버로딩 중에서 강의하지 않은 연산자가 있다. () - 함수 호출 연산자
 	
 	Dog dogs[10];	// Dog는 생성시 [10,30] 임의의 길이를 갖는 STRING name멤버가 있다. 
-				// name은 생성시 랜덤 소문자로 초기화된다. 
+					// name은 생성시 랜덤 소문자로 초기화된다. 
+					// 생성시 [1,100'0000] 랜덤 id값을 갖는다.
 
 	// [문제] Dog를 name 길이 기준 오름차순으로 정렬하라 
 	qsort(dogs, 10, sizeof(Dog), [](const void* a, const void* b) {
-		return (int)((*(Dog*)a).nameSize() - (*(Dog*)b).nameSize());
+		return (int)(((Dog*)a)->nameSize() - ((Dog*)b)->nameSize());
 		});
-	 
+	
 
 	for (const Dog& dog : dogs) {
-		std::cout << dog << std::endl;
-	}
+		dog();			// id와 name을 화면에 출력하라.
+	}					// [id:8자리] - name
 
 	
 	//save("main.cpp");
