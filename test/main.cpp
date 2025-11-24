6308d62cbc1e39ec4af37a3cd309f6315e2549f9
THe-L00t <sigun2001@naver.com>
2025-11-24 12:49:53 +0900

    [문제] 다운받은 파일 "개천마리.txt"에는 class Dog 객체 1000개가 저장되어 있다.

--- test/main.cpp
+++ test/main.cpp
@@ -13,8 +13,24 @@
 using namespace std;
 extern bool observe;
 
+// [문제] 다운받은 파일 "개천마리.txt"에는 class Dog 객체 1000개가 저장되어 있다. 
+// 파일을 본것과 같이 id
+// 파일을 모두 읽어 Dog객체에 저장하라. 
+// 읽은 Dog객체를 id기준 오름차순 정렬하라. 
+// 화면에 id와 name를 출력하라. 
+class Dog {
+public:
+	friend std::istream& operator>>(std::istream& is, Dog& rhs) {
+		return is >> rhs.id >> rhs.name;
+	}
+private:
+	int id{};
+	std::string name{};
+};
+
 int main()
 {
+	Dog dogs[1000];
 
 	
 	//save("main.cpp");
