aeaad45434b9ec01a3766e50cbc4090832820b5b
THe-L00t <sigun2001@naver.com>
2025-12-11 17:55:51 +0900

    down casting을 위한 dynamic_cast

--- test/main.cpp
+++ test/main.cpp
@@ -65,7 +65,7 @@ int main()
 
 	Game* g[2]{ &h,&m };
 	// 내가 관리하는 Game 객체들의 render를 호출
-	// 그런데 Human일때만 render 하고 싶다. (up casting은 언제나 문제 없다 )
+	// 그런데 Human일때만 render 하고 싶다. (up casting은 언제나 문제 없다 / down casting을 위한 dynamic_cast )
 	for (Game* obj : g) {
 		obj->render();
 	}
